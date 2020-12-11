2a3671254e12ba6c3051388956b3c3c8c4b1d4e9
YOUR NAME <mariem.chalghaf1992@gmail.com>
2020-12-11 15:51:27 +0100

    projet

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var a=document.getElementById(ico1)
+var b=document.getElementById(ico2)
+var c=document.getElementById(ico3)
+
+
+function heart1(){
+    if(a.style.color=='red'){
+      a.style.color='black'
+    }
+    else{
+      a.style.color='red'
+    }
+  }
+
+
+
+  function heart2(){
+    if(b.style.color=='red'){
+      b.style.color='black'
+    }
+    else{
+      b.style.color='red'
+    }
+  }
+
+
+  function heart3(){
+    if(c.style.color=='red'){
+      c.style.color='black'
+    }
+    else{
+      c.style.color='red'
+    }
+  }
+
+
